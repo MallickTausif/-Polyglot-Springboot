b4e605d246d1cc665314fe4cdf6ca75fb39dfc9f
MallickTausif <168614994+MallickTausif@users.noreply.github.com>
2024-11-21 03:27:39 +0530

    Update PyglotApplication.java

--- src/main/java/com/polyglot/PyglotApplication.java
+++ src/main/java/com/polyglot/PyglotApplication.java
@@ -1,6 +1,6 @@
 package com.polyglot;
 
-import com.polyglot.services.CourseService; // Ensure the correct package and capitalization for the service
+import com.polyglot.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +9,16 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class PyglotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PyglotApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PyglotApplication.class, args);
+	}
 
-    @Bean
-    public CommandLineRunner dataLoader(CourseService courseService) { // Fix capitalization of parameter
-        return args -> {
-            // Add data to the database
-            courseService.addCourses();
-        };
-    }
-}
+	@Bean
+	public CommandLineRunner dataLoader(courseService coursesService) {
+		return args -> {
+			// Add data to the database
+			coursesService.addCourses();
+		};
+	}
+
+}Correct if some error
